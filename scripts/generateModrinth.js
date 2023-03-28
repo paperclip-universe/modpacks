0627b4ba60e65c7b85408f1e00614c399cc29538
Wyatt Stanke <47758296+Wyatt-Stanke@users.noreply.github.com>
2023-03-28 16:41:40 -0400

    add autobuilding

--- scripts/generateModrinth.js
+++ scripts/generateModrinth.js
@@ -9,18 +9,19 @@ import { SingleBar, Presets } from "cli-progress";
 // 	Presets.shades_classic,
 // );
 
+$.verbose = false;
 let packVersion = await $`cat .version`.quiet();
 const versions = await fs.readdir("quilt/");
 await $`mkdir -p out/`;
-// bar.start(versions.length, 0);
+bar.start(versions.length, 0);
 for (const folder of versions) {
 	if (folder === ".DS_Store") continue; // Macos moment
-	// bar.update({ version: folder });
+	bar.update({ version: folder });
 	cd(`quilt/${folder}`);
 	await $`rm -rf *.mrpack`;
 	await $`packwiz modrinth export -y`;
 	await $`mv *.mrpack "Redstonality v${packVersion} for ${folder}.mrpack"`;
 	await $`mv *.mrpack ../../out/`;
 	cd("../..");
-	// bar.increment();
+	bar.increment();
 }
--- scripts/syncMods.js
+++ scripts/syncMods.js
@@ -1,37 +1,60 @@
 #!/usr/bin/env zx
 import { SingleBar, Presets } from "cli-progress";
+import { versions } from "./versions.js";
+
+$.verbose = false;
 
 // Get full modlist from fullModList.txt
-let modlist = await $`cat fullModList.txt`.quiet();
+let modlist = await $`cat fullModList.txt`;
 modlist = modlist.stdout
 	.split("\n")
 	.filter((x) => x.length > 0 && !x.startsWith("#"))
 	.map((mod) => mod.split("/"));
-echo("");
-for (const folder of await fs.readdir("quilt/")) {
-	cd(`quilt/${folder}`);
-	const bar = new SingleBar(
-		{
-			modname: folder,
-			format: " {bar} | {value}/{total} | {modname} | {version} | ETA: {eta}s",
-		},
-		Presets.shades_classic,
-	);
-	bar.start(modlist.length, 0, { modname: "Starting...", version: folder });
+const bar = new SingleBar(
+	{
+		format:
+			" {bar} | {value}/{total} ({valid}) | {modname} | {version} | {lastStatus} | ETA: {eta}s",
+	},
+	Presets.shades_classic,
+);
+
+bar.start(versions.length * modlist.length, 0, {
+	modname: `${modlist[0][0]}/${modlist[0][1]}`,
+	version: versions[0],
+	lastStatus: "OK",
+	valid: 0,
+});
+
+for (const version of versions) {
+	cd(`quilt/${version}`);
+	let valid = 0;
 	for (const mod of modlist) {
 		bar.update({ modname: `${mod[0]}/${mod[1]}` });
-		await $`packwiz ${mod[0]} install ${mod[1]} -y`.quiet().catch((e) => {
-			// Check if rate-limited
-			if (e.stderr.includes("429")) {
-				sleep(60000);
-				return $`packwiz ${mod[0]} install ${mod[1]} -y`.catch((_) => {});
-			} else {
-				console.log(e);
-			}
-		});
+		await $`packwiz ${mod[0]} install ${mod[1]} -y`
+			.quiet()
+			.then(() => bar.update({ valid: (valid += 1) }))
+			.catch(async (e) => {
+				if (
+					e.stdout.includes("no valid versions found") |
+					e.stdout.includes(
+						"mod not available for the configured Minecraft version",
+					)
+				) {
+					bar.update({ lastStatus: "No valid versions found" });
+				} else if (e.stdout.includes("429")) {
+					bar.update({ lastStatus: "Rate limited" });
+					await sleep(60000);
+					return await $`packwiz ${mod[0]} install ${mod[1]} -y`.catch(
+						(_) => {},
+					);
+				} else {
+					console.log(e);
+				}
+			});
 		bar.increment();
 	}
-	bar.stop();
 	await sleep(1000);
 	cd("../..");
 }
+
+bar.stop();
