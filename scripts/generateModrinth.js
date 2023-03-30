77875dc2f51b5ae689164733f80d018f8d548ae0
Wyatt-Stanke <wyattstanke@icloud.com>
2023-03-30 14:42:46 +0000

    fjallbo

--- scripts/generateModrinth.js
+++ scripts/generateModrinth.js
@@ -4,8 +4,9 @@ import { versions, loaders } from "./consts.js";
 
 const bar = new SingleBar(
 	{
-		format: " {bar} | {value}/{total} | {version} | ETA: {eta}s",
+		format: " {bar} | {value}/{total} | {version} | {loader} | ETA: {eta}s",
 		version: "Starting...",
+		loader: "Starting...",
 	},
 	Presets.shades_classic
 );
@@ -13,14 +14,14 @@ const bar = new SingleBar(
 $.verbose = false;
 let packVersion = await $`cat .version`.quiet();
 await $`mkdir -p out/`;
-bar.start(versions.length, 0);
+bar.start(versions.length * loaders.length, 0);
 for (const version of versions) {
 	for (const loader of loaders) {
-		bar.update({ version: version });
-		cd(`${loaders}/${version}`);
+		bar.update({ version, loader });
+		cd(`${loader}/${version}`);
 		await $`rm -rf *.mrpack`;
 		await $`packwiz modrinth export -y`;
-		await $`mv *.mrpack "Redstonality v${packVersion} for ${version}.mrpack"`;
+		await $`mv *.mrpack "Redstonality v${packVersion} for ${version} via ${loader}.mrpack"`;
 		await $`mv *.mrpack ../../out/`;
 		cd("../..");
 		bar.increment();
